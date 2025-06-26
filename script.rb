af94a06b7a7fd079c795c10d72848430ded0345c
cookingnoob <{ID}+{username}@users.noreply.github.com>
2025-06-26 17:30:46 -0600

    add function that separates the string into an array of words which can be looped through

--- script.rb
+++ script.rb
@@ -1,5 +1,14 @@
 uppercase_alphabet = ('A'..'Z').to_a
 lowercase_alphabet = ('a'..'z').to_a
 
-p uppercase_alphabet
-p lowercase_alphabet
+p uppercase_alphabet[10]
+p lowercase_alphabet[10]
+
+def cipher (string)
+  string_array = string.split("")
+  string_array.map do |letter|
+    p letter
+  end
+end
+
+cipher('hi how are you')
