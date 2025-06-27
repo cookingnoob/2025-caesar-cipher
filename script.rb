2d663b79ddc681f984939b2bc319f2175632c9b3
cookingnoob <{ID}+{username}@users.noreply.github.com>
2025-06-26 19:04:26 -0600

    add if statment to check if letter is capital or not

--- script.rb
+++ script.rb
@@ -3,12 +3,17 @@
 def cipher (string, shift)
   string_array = string.split("")
   new_string_array = string_array.map do |letter|
+    if letter.upcase == letter
+      letter_index = ('A'..'Z').to_a.find_index {|l| l == letter}  
+    ('A'..'Z').to_a[letter_index + shift]
+    else
     letter_index = ('a'..'z').to_a.find_index {|l| l == letter}  
     ('a'..'z').to_a[letter_index + shift]
+    end
   end
   new_string_array.join
 end
 
 
 
-p cipher('hello', 5)
+p cipher('HelloOOo', 5)
