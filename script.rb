93345aa84ac7a141cba65dca2abc6d529b0349c9
cookingnoob <{ID}+{username}@users.noreply.github.com>
2025-07-02 13:28:00 -0600

    refactor cipher to join the result of three methods that work with the initial array, add rubocop autocorrections

--- script.rb
+++ script.rb
@@ -1,23 +1,35 @@
-def get_cipher(first, last, letter, shift)
+# frozen_string_literal: true
+
+def change_letter(first, last, letter, shift)
   alphabet = (first..last).to_a
-  letter_index = alphabet.find_index {|l| l == letter}  
+  letter_index = alphabet.find_index { |l| l == letter }
   alphabet[(letter_index + shift) % alphabet.length]
 end
 
-def cipher (string, shift)
-  string_array = string.split("")
-  new_string_array = string_array.map do |letter|
-    if letter == " " || letter == "!"
-      letter
-    elsif letter.upcase == letter
-      get_cipher('A', 'Z',letter, shift)
-    elsif letter.downcase == letter
-      get_cipher('a', 'z',letter, shift)
-    end
-  end
-  new_string_array.join
+def get_array(string)
+  string.split('')
 end
 
+def map_array(array, shift)
+  array.map do |letter|
+    if [' ', '!'].include?(letter)
+      letter
+    elsif letter.upcase == letter
+      change_letter('A', 'Z', letter, shift)
+    elsif letter.downcase == letter
+      change_letter('a', 'z', letter, shift)
+    end
+  end
+end
 
+def join_array(array)
+  array.join
+end
+
+def cipher(string, shift)
+  string_array = get_array(string)
+  new_string_array = map_array(string_array, shift)
+  join_array(new_string_array)
+end
 
 p cipher('AaaBbb xyzo!!', 1)
